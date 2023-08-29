f7c90a1db3a4183dbbcb91e012fe27a6b41b89b8
da-nazy <danielani660@gmail.com>
2023-08-29 17:11:12 +0100

    feat:prisma crud

---
+++ prisma/migrations/20230829160928_second/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 0,
+    "price" INTEGER NOT NULL DEFAULT 999,
+    "cratedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "categoryId" INTEGER NOT NULL,
+    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Product" ("categoryId", "cratedAt", "id", "name", "price") SELECT "categoryId", "cratedAt", "id", "name", "price" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
---
+++ routes/api.route.js
@@ -0,0 +1,86 @@
+const router = require('express').Router();
+const {PrismaClient}=require('@prisma/client');
+
+const prisma=new PrismaClient();
+
+router.get('/products', async (req, res, next) => {
+ try{
+   const products=await prisma.product.findMany({
+    include:{
+      category:true
+    }
+   });
+
+   const categories=await prisma.category.findMany({
+    include:{products:true}
+   })
+
+   return res.json({products,categories});
+
+ }catch(error){
+  next(error)
+ }
+});
+
+router.get('/products/:id', async (req, res, next) => {
+  try{
+const {id}=req.params;
+const product=await prisma.product.findUnique({
+  where:{
+    id:Number(id)
+  },
+  include:{category:true}
+})
+console.log(product.quantity);
+   res.json(product);
+  }catch(error){
+    next(error);
+  }
+});
+router.post('/products', async (req, res, next) => {
+  try{
+   const data=req.body;
+   const product=await prisma.product.create({
+    data:req.body
+   })
+   res.json(product);
+  }catch(error){
+    next(error);
+  }
+});
+router.delete('/products/:id', async (req, res, next) => {
+  try{
+    const {id}=req.params;
+    const deletedProduct=await prisma.product.delete({
+      where:{
+        id:Number(id)
+      }
+    })
+   
+    return res.json(deletedProduct);
+ 
+  }catch(error){
+   next(error)
+  }
+ 
+});
+router.patch('/products/:id', async (req, res, next) => {
+ try{
+  const {id}=req.params;
+  const product=await prisma.product.update({
+    where:{
+      id:Number(id)
+    },
+    data:req.body,
+    include:{
+      category:true
+    }
+  })
+  res.json(product);
+  
+ }catch(error){
+  next(error)
+ }
+});
+
+module.exports = router;
